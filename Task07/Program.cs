34ec594f116ab2eb310ec8cd6396176b7fbd0c41
pavelnagornov <paul.nagornov@gmail.com>
2022-12-19 11:36:45 +0500

    add Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num <= 999)
+{
+    Console.WriteLine($"Последняя цифра числа {num % 10}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное!");
+}
