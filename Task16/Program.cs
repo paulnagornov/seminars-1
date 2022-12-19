566144c6ab5e454f2bd6671c87669d6c0bdea9df
pavelnagornov <paul.nagornov@gmail.com>
2022-12-19 14:36:22 +0500

    add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(IsSquare(number1, number2) ? "да" : "нет");
+bool IsSquare(int n1, int n2)
+{
+    return n1 / n2 == n2 || n2 / n1 == n1;
+}
+
+
+
+
+
