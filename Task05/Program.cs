d11e9b5bbfaf121f68710af28b521ace284ff903
pavelnagornov <paul.nagornov@gmail.com>
2022-12-19 11:18:20 +0500

    add Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = -number;
+while (counter <= number)
+{
+    Console.Write(counter + " ");
+    counter++;
+}
