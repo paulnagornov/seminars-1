f5681d73cce21a433ab1f647d8a53fa3b795e1fc
pavelnagornov <paul.nagornov@gmail.com>
2022-12-19 14:21:54 +0500

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Число делится на 23 и на 7");
+}
+else
+{
+    Console.WriteLine("Число не делится ");
+}
