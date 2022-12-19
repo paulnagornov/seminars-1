3f88a0822a6b4476a6ea922dd6258e4731c385ee
pavelnagornov <paul.nagornov@gmail.com>
2022-12-19 09:42:52 +0500

    add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое целое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine($"Первое число {a} является квадратом числа {b}");
+}
+else
+{
+    Console.WriteLine($"Первое число {a} не является квадратом числа {b}");
+}
