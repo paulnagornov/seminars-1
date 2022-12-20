d36c73bacafd0340cd606dce86a2f8b21df130b9
pavelnagornov <paul.nagornov@gmail.com>
2022-12-20 21:16:14 +0500

    add changes Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -6,48 +6,22 @@
 // 16, 4 -> кратно
 
 
-int number1 = new Random().Next(1, 100);
-int number2 = new Random().Next(1, 10);
-Console.WriteLine("Введите два числа");
-int div = number1 % number2;
-if (div == 0)
-{
-    Console.WriteLine("Первое число кратно второму");
-}
-else
-{
-    Console.WriteLine($"Первое число не кратно второму, остаток {div}");
-}
-
-
-
-
-// int DeleteSecond(int num)
-// {
-//     int firstDigit = num / 100;
-//     int thirdDigit = num % 10;
-//     return firstDigit * 10 + thirdDigit; 
-// }
-// Console.WriteLine(DeleteSecond(number));
-
-
-
 Console.WriteLine("Введите число:");
-int userNumber = Convert.ToInt32(Console.ReadLine());
+int userNum1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число:");
-int userNumber2 = Convert.ToInt32(Console.ReadLine());
+int userNum2 = Convert.ToInt32(Console.ReadLine());
 
-bool DivCheck(int number1, int number2)
+bool DivCheck(int num1, int num2)
 {
-    return number1 % number2 == 0;
+    return num1 % num2 == 0;
 }
 
-if (DivCheck(userNumber, userNumber2))
+if (DivCheck(userNum1, userNum2))
 {
-    Console.WriteLine($"{userNumber}, {userNumber2} -> кратно");
+    Console.WriteLine($"{userNum1}, {userNum2} -> кратно");
 }
 else
 {
-    Console.WriteLine($"{userNumber}, {userNumber2} -> не кратно, остаток " + (userNumber % userNumber2));
+    Console.WriteLine($"{userNum1}, {userNum2} -> не кратно, остаток " + (userNum1 % userNum2));
 }
