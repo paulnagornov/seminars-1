b50eb0efc31bb1679a81dc13201147382eb2b3fa
pavelnagornov <paul.nagornov@gmail.com>
2022-12-22 18:59:03 +0500

    changes Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -8,13 +8,21 @@
 
 Console.WriteLine("Введите число от 1 до 7");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
-int CheckDay(int day)
+
+CheckDay(dayNumber);
+
+void CheckDay(int dayNum)
 {
-    int day1 = Console.WriteLine("Понедельник");
-    int day2 = Console.WriteLine("Вторник");
-    int day3 = Console.WriteLine("Среда");
-    int day4 = Console.WriteLine("Четверг");
-    int day5 = Console.WriteLine("Пятница");
-    int day6 = Console.WriteLine("Суббота - выходной");
-    int day7 = Console.WriteLine("Воскресенье - выходной");
+    if (dayNum == 6 || dayNum == 7)
+    {
+        Console.WriteLine("Выходной день");
+    }
+    else if (dayNum < 1 || dayNum > 7)
+    {
+        Console.WriteLine("Ошибка ввода");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
 }
