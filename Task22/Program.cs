b56ee9c5209eff5a802e19fa9073881fd0fa7556
pavelnagornov <paul.nagornov@gmail.com>
2022-12-22 14:12:57 +0500

    add Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+SquareNum(num);
+
+void SquareNum(int number)
+{
+    int index = 1;
+    while (index <= number)
+    {
+        Console.WriteLine($"{index} -> {index * index}");
+        index++;
+    }
+}
