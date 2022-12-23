8781e78857062a33643adb8342bead7a2df4694d
pavelnagornov <paul.nagornov@gmail.com>
2022-12-23 18:50:20 +0500

    changes Task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -3,17 +3,39 @@
 // квадратов чисел от 1 до N.
 
 
-Console.WriteLine("Введите натуральное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите натуральное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-SquareNum(num);
+// SquareNum(num);
 
-void SquareNum(int number)
+// void SquareNum(int number)
+// {
+//     int index = 1;
+//     while (index <= number)
+//     {
+//         Console.WriteLine($"{index} -> {index * index}");
+//         index++;
+//     }
+// }
+
+// С проверкой на отрицательное.
+
+
+void Numb(int n1)
 {
-    int index = 1;
-    while (index <= number)
+    int counter = 1;
+    while (counter <= n1)
     {
-        Console.WriteLine($"{index} -> {index * index}");
-        index++;
+        Console.WriteLine($"{counter, 3} {counter * counter, 3}");
+        counter++;
     }
 }
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number < 1)
+{
+    Console.WriteLine("Введите положительное, целое число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+Numb(number);
