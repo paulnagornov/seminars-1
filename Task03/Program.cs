638a3ed88c26ceb146d1e1b7628bd8a6dfce1a66
pavelnagornov <paul.nagornov@gmail.com>
2022-12-23 21:59:17 +0500

    changes Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,33 +1,33 @@
 ﻿Console.WriteLine("Введите число от 1 до 7:");
-string day = Console.ReadLine();
+int day = Convert.ToInt32(Console.ReadLine());
 
-if (day == "1")
+if (day == 1)
 {
     Console.WriteLine("Понедельник");
 }
-else if (day == "2")
+else if (day == 2)
 {
     Console.WriteLine("Вторник");
 }
-else if (day == "3")
+else if (day == 3)
 {
     Console.WriteLine("Среда");
 }
-else if (day == "4")
+else if (day == 4)
 {
     Console.WriteLine("Четверг");
 }
-else if (day == "5")
+else if (day == 5)
 {
     Console.WriteLine("Пятница");
 }
-else if (day == "6")
+else if (day == 6)
 {
     Console.WriteLine("Суббота");
 }
-else if (day == "7")
+else if (day == 7)
 {
-    Console.WriteLine("Воскресенбе");
+    Console.WriteLine("Воскресенье");
 }
 else
 {
