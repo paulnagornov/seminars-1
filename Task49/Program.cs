25a2bdc4f9afc601d99b513438e8fb636f5db8da
pavelnagornov <paul.nagornov@gmail.com>
2023-01-23 13:56:40 +0500

    Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 2
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //
+        {
+            matrix[i, j] = rnd.Next(min, max + 1); // 2 - 3
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j], 5},");
+            else
+                Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void SquareMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2D);
+SquareMatrix(array2D);
+Console.WriteLine();
+PrintMatrix(array2D);
