5f88bbe62661a433bf2fd7379f68293df31b332c
pavelnagornov <paul.nagornov@gmail.com>
2023-01-30 14:51:53 +0500

    Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    if(num == 0) return 0;
+    return num % 10 + SumNumbers(num / 10);
+}
+int num = SumNumbers(number);
+Console.WriteLine(num);
+
+
+
+
+
+
+// int Factorial(int n)
+// {
+//     if (n == 1)
+//         return 1;
+//     else
+//         return n * Factorial(n - 1);
+// }
